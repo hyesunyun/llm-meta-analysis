1207a5587a07c59d1be661ba14337fce59177ad6
hyesunyun <hyesunyun09@gmail.com>
2024-02-01 14:16:18 -0500

    Add more templates

--- evaluation/task_runner.py
+++ evaluation/task_runner.py
@@ -12,11 +12,13 @@ from tqdm import tqdm
 from templates import DatasetTemplates
 from models import Model, GPT35, GPT4
 from datetime import datetime
+import random
 
 class MetaAnalysisTaskRunner:
-    def __init__(self, model_name: str, task: str, output_path: str, is_test: bool, prompt_name: Optional[str]=None) -> None:
+    def __init__(self, model_name: str, task: str, split: str, output_path: str, is_test: bool, prompt_name: Optional[str]=None) -> None:
         self.model_name = model_name
         self.task = task
+        self.split = split
         self.prompt_name = prompt_name
         self.output_path = output_path
         self.is_test = is_test
@@ -48,12 +50,17 @@ class MetaAnalysisTaskRunner:
         dataset_filename = "meta_analysis_dataset.json"
         dataset = load_dataset_from_json(dataset_filename)
 
-        # if test, only get the dev set since this is for debugging and running quick experiments
-        if self.is_test:
+        # get the correct split to run the task
+        if self.split == "dev":
             dataset = [example for example in dataset if example["split"] == "DEV"]
         else:
             # filter out dataset to only test (evaluation) split
             dataset = [example for example in dataset if example["split"] == "TEST"]
+
+        # if test, only get 10 random examples
+        if self.is_test:
+            random.shuffle(dataset)
+            dataset = dataset[:10]
 
         # Add xml content to each example
         for example in dataset:
@@ -80,51 +87,52 @@ class MetaAnalysisTaskRunner:
 
         # if prompt name is given, apply the prompt template. if not, use all 
         if self.prompt_name is None:
-            pbar_prompts = tqdm(prompts.all_template_names)
+            all_prompt_templates = prompts.all_template_names
+            prompt_template_name = all_prompt_templates[0]
         else:
-            pbar_prompts = tqdm([self.prompt_name])
+            prompt_template_name = self.prompt_name
         
-        # run task for each prompt template
-        for _, prompt_name in enumerate(pbar_prompts):
-            pbar_prompts.set_description_str(f"Running model inference using template/prompt name: {prompt_name}")
-            
-            prompt = prompts[prompt_name]
+        prompt = prompts[prompt_template_name]
 
-            dataset = [format_example_with_prompt_template(example, prompt) for example in tqdm(self.dataset)]
+        # format the dataset with the prompt template
+        dataset = [format_example_with_prompt_template(example, prompt) for example in tqdm(self.dataset)]
 
-            results = []
-            pbar = tqdm(dataset)
-            for _, example in enumerate(pbar):
-                output = self.model.generate_output(example["input"])
-                example["output"] = output
-                results.append(example)
+        # run the task using specified model
+        results = []
+        pbar = tqdm(dataset)
+        for _, example in enumerate(pbar):
+            output = self.model.generate_output(example["input"])
+            example["output"] = output
+            results.append(example)
 
-            # saving results to file
-            print(f"Saving outputs for task - {self.task}; prompt - {prompt_name}; model - {self.model_name} to csv and json")
-            current_datetime = datetime.now().strftime("%Y%m%d")
+        # saving results to file
+        print(f"Saving outputs for task - {self.task}; prompt - {prompt_name}; model - {self.model_name} to csv and json")
+        current_datetime = datetime.now().strftime("%Y%m%d")
 
-            # convert into json
-            json_file_path = f"{self.output_path}/{self.model_name}_{self.task}_output_{current_datetime}.json"
-            save_dataset_to_json(dataset, json_file_path)
+        # convert into json
+        json_file_path = f"{self.output_path}/{self.model_name}_{self.task}_output_{current_datetime}.json"
+        save_dataset_to_json(dataset, json_file_path)
 
-            # convert into csv
-            csv_file_path = f"{self.output_path}/{self.model_name}_{self.task}_output_{current_datetime}.csv"
-            save_dataset_to_csv(dataset, csv_file_path)
+        # convert into csv
+        csv_file_path = f"{self.output_path}/{self.model_name}_{self.task}_output_{current_datetime}.csv"
+        save_dataset_to_csv(dataset, csv_file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Running Clinical Trials Meta Analysis Task")
 
     parser.add_argument("--model", default="gpt35", choices=["gpt35", "gpt4"], help="what model to run", required=True)
     parser.add_argument("--task", default="outcome_type", choices=['outcome_type', 'binary_outcomes', 'continuous_outcomes'], help="type of task to run", required=True)
-    parser.add_argument("--prompt", default=None, help="specific prompt to run. if no specific prompt is given, all prompts related to given task are run. OPTIONAL")
+    parser.add_argument("--split", default="test", choices=["test", "dev"], help="which split of the dataset to run")
+    parser.add_argument("--prompt", default=None, help="specific prompt to run. if no specific prompt is given, the first prompt for the given task is run. OPTIONAL")
     parser.add_argument("--output_path", default="./output", help="directory of where the outputs/results should be saved")
     # do --no-test for explicit False
-    parser.add_argument("--test", action=argparse.BooleanOptionalAction, help="whether this is a test run or not. test will only run dev instances from the dataset.")
+    parser.add_argument("--test", action=argparse.BooleanOptionalAction, help="used for debugging purposes. test will only run random 10 instances from the dataset.")
     
     args = parser.parse_args()
 
     model = args.model
     task = args.task
+    split = args.split
     prompt_name = args.prompt
     output_path = args.output_path
     is_test = args.test
@@ -132,6 +140,7 @@ if __name__ == '__main__':
     print("Arguments for the Clinical Trials Meta Analysis Task Runner:")
     print(f"model: {model}")
     print(f"task: {task}")
+    print(f"split: {split}")
     print(f"prompt_name: {prompt_name}")
     print(f"output_path: {output_path}")
     print(f"is_text: {is_test}")
@@ -140,6 +149,6 @@ if __name__ == '__main__':
         os.makedirs(output_path)
         print("output path did not exist. directory was created.")
 
-    task_runner = MetaAnalysisTaskRunner(model, task, output_path, is_test, prompt_name)
+    task_runner = MetaAnalysisTaskRunner(model, task, split, output_path, is_test, prompt_name)
     task_runner.run_task()
     
