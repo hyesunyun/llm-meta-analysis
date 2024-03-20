537d15902ac56d4541e510179376ece970157557
hyesunyun <hyesunyun09@gmail.com>
2024-03-20 15:55:50 -0400

    update open source testing

---
+++ evaluation/run_meta_analysis_example.py
@@ -0,0 +1,7 @@
+# run case study
+# https://www.cochranelibrary.com/cdsr/doi/10.1002/14651858.CD014962/references#dataAndAnalyses
+# mortality outcome and 4 RCTs
+# manually extracting the RCT text
+# show how well the model can extract the data and perform the meta-analysis. compare with the cochrane results
+
+# add forest plot too
