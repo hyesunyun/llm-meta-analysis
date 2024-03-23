fc2e38fc2c92ad72fda4431cbdd4a93b93b7db19
hyesunyun <hyesunyun09@gmail.com>
2024-03-23 11:39:49 -0400

    fix decoding bug

--- evaluation/models/gemma.py
+++ evaluation/models/gemma.py
@@ -39,6 +39,6 @@ class Gemma(Model):
             inputs = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors="pt").to(self.device)
             with torch.no_grad():
                 result = self.model.generate(inputs, max_new_tokens=max_new_tokens)
-            return self.tokenizer.decode(result[0, inputs.input_ids.shape[1]:], skip_special_tokens=True)
+            return self.tokenizer.decode(result[0, inputs.shape[1]:], skip_special_tokens=True)
         except Exception as e:
             print("[ERROR]", e)
