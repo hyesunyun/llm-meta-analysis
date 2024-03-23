96f694133f3ac19706e162588f04a53567ecfbfd
hyesunyun <hyesunyun09@gmail.com>
2024-03-23 11:23:14 -0400

    fix mistral generate

--- evaluation/models/mistral.py
+++ evaluation/models/mistral.py
@@ -19,8 +19,8 @@ class Mistral(Model):
         return model
 
     def load_tokenizer(self):
-        tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", padding_side="left")
-        tokenizer.pad_token = tokenizer.eos_token  # Most LLMs don't have a pad token by default
+        tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+        # tokenizer.pad_token = tokenizer.eos_token  # Most LLMs don't have a pad token by default
         return tokenizer
 
     def generate_output(self, input: str, max_new_tokens: int) -> str:
@@ -40,6 +40,6 @@ class Mistral(Model):
             model_inputs = encoded.to(self.device)
             with torch.no_grad():
                 result = self.model.generate(model_inputs, max_new_tokens=max_new_tokens)
-            return self.tokenizer.decode(result[0, model_inputs.input_ids.shape[1]:], skip_special_tokens=True)
+            return self.tokenizer.decode(result[0, model_inputs.shape[1]:], skip_special_tokens=True)
         except Exception as e:
             print("[ERROR]", e)
