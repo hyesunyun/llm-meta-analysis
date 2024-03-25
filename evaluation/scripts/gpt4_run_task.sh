9a7db48a7cc8ba31f9e25feb9f5fbb2dc608831a
hyesunyun <hyesunyun09@gmail.com>
2024-03-25 09:01:54 -0400

    adding gpt-4 scripts

---
+++ evaluation/scripts/gpt4_run_task.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=64:00:00
+#SBATCH --job-name=gpt4
+#SBATCH --mem=45G
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=frink
+#SBATCH --output=exec.gpt4.%j.evaluate.out
+
+module load anaconda3/2022.05
+
+source activate llm-meta-analysis
+
+export $(xargs < ../../.env)
+
+# outcome_types
+python3 ../run_task.py --model gpt4 --task outcome_types --split test --prompt without-abstract-results --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_types
+
+# binary outcomes
+python3 ../run_task.py --model gpt4 --task binary_outcomes --split test --prompt yaml --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes
+
+# continuous_outcomes
+python3 ../run_task.py --model gpt4 --task continuous_outcomes --split test --prompt yaml --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes
