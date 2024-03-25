43717b475c0914b6f9d7eec4acdc09343e6c1262
hyesunyun <hyesunyun09@gmail.com>
2024-03-25 17:27:20 -0400

    add some results

--- evaluation/scripts/gpt4_run_eval.sh
+++ evaluation/scripts/gpt4_run_eval.sh
@@ -17,7 +17,7 @@ export $(xargs < ../../.env)
 # python3 ../evaluate_output.py --task outcome_type --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type/gpt4_outcome_type_test_output_20240325-11:55:44.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/outcome_type/
 
 # binary outcomes
-python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/gpt4_binary_outcomes_test_output_20240325-12:19:11.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/
+python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/gpt4_binary_outcomes_test_output_20240325-16:51:52.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/
 
 # continuous_outcomes
-python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/gpt4_continuous_outcomes_test_output_20240325-13:46:14.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes
+# python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/gpt4_continuous_outcomes_test_output_20240325-13:46:14.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes
--- evaluation/scripts/gpt4_run_task.sh
+++ evaluation/scripts/gpt4_run_task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=64:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=gpt4
 #SBATCH --mem=45G
 #SBATCH --cpus-per-task=4
@@ -17,7 +17,7 @@ export $(xargs < ../../.env)
 # python3 ../run_task.py --model gpt4 --task outcome_type --split test --prompt without-abstract-results --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type
 
 # binary outcomes
-python3 ../run_task.py --model gpt4 --task binary_outcomes --split test --prompt yaml --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes
+# python3 ../run_task.py --model gpt4 --task binary_outcomes --split test --prompt yaml --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes
 
 # continuous_outcomes
 python3 ../run_task.py --model gpt4 --task continuous_outcomes --split test --prompt yaml --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes
