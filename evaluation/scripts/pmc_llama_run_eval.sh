e6da0f19ffc17963fc910f444f6479116df93cb5
hyesunyun <hyesunyun09@gmail.com>
2024-03-26 15:39:59 -0400

    add eval sh

--- evaluation/scripts/pmc_llama_run_eval.sh
+++ evaluation/scripts/pmc_llama_run_eval.sh
@@ -13,7 +13,7 @@ source activate llm-meta-analysis
 export $(xargs < ../../.env)
 
 # outcome_types
-python3 ../evaluate_output.py --task outcome_type --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type/ --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/outcome_type/
+python3 ../evaluate_output.py --task outcome_type --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type/pmc-llama_outcome_type_test_output_20240326-14:21:40.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/outcome_type/
 
 # binary outcomes
 # python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/ --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/
