1fa9be3c04afd65ce9b65e28dca2c83bbd5f0d4c
hyesunyun <hyesunyun09@gmail.com>
2024-04-10 14:01:18 -0400

    xml metrics

--- evaluation/scripts/pmc_llama_run_eval.sh
+++ evaluation/scripts/pmc_llama_run_eval.sh
@@ -16,7 +16,7 @@ export $(xargs < ../../.env)
 # python3 ../evaluate_output.py --task outcome_type --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type/pmc-llama_outcome_type_test_output_20240326-14:21:40.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/outcome_type/
 
 # binary outcomes
-python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/xml/pmc-llama_binary_outcomes_test_output_20240331-22:04:16.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/xml/
+# python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/xml/pmc-llama_binary_outcomes_test_output_20240331-22:04:16.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/xml/
 
 # continuous_outcomes
-python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/xml/pmc-llama_continuous_outcomes_test_output_20240401-18:10:38.jsonpmc-llama_continuous_outcomes_test_output_20240401-18:10:38.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes/xml/
+python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/xml/pmc-llama_continuous_outcomes_test_output_20240401-18:10:38.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes/xml/
