05209506ee0659e2bada7a40887a8b4c8a364553
hyesunyun <hyesunyun09@gmail.com>
2024-04-10 11:28:53 -0700

    adding all metrics

--- evaluation/scripts/gpt35_run_eval.sh
+++ evaluation/scripts/gpt35_run_eval.sh
@@ -16,7 +16,7 @@ export $(xargs < ../../.env)
 # python3 ../evaluate_output.py --task outcome_type --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/outcome_type/gpt35_outcome_type_test_output_20240326-11:38:47.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/outcome_type/
 
 # binary outcomes
-python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/gpt35_binary_outcomes_test_output_20240403-12:38:47.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/
+python3 ../evaluate_output.py --task binary_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/binary_outcomes/xml/gpt35_binary_outcomes_test_output_20240331-19:07:00.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/binary_outcomes/xml/
 
 # continuous_outcomes
-python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/gpt35_continuous_outcomes_test_output_20240403-13:10:49.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes
+python3 ../evaluate_output.py --task continuous_outcomes --output_path /scratch/yun.hy/llm-meta-analysis/evaluation/outputs/continuous_outcomes/xml/gpt35_continuous_outcomes_test_output_20240331-19:40:28.json --metrics_path /scratch/yun.hy/llm-meta-analysis/evaluation/metrics/continuous_outcomes/xml/
